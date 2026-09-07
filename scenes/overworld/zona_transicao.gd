7e501a57c9cae402502b3f5b0e87f7391a2f5109
unknown <victor.coutomonteiro@ufape.edu.br>
2026-09-07 16:54:46 -0300

    resolvendo problemas finais

---
+++ scenes/overworld/zona_transicao.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+enum TipoTransicao {
+	CONTIGUA,
+	DISCRETA
+}
+
+@export_file("*.tscn") var cena_destino: String
+
+@export var tipo_transicao: TipoTransicao = TipoTransicao.CONTIGUA
+
+@export_group("Discreta")
+@export var spawn_point_id: String 
+
+@export_group("Contigua")
+@export var offset_matematico: Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	# Conecta o sinal de colisão automaticamente
+	area_entered.connect(_ao_encostar_no_portal)
+	
+
+func _ao_encostar_no_portal(area: Area2D) -> void:
+	# Verifica se quem bateu no portal foi o jogador
+	pass
+	
